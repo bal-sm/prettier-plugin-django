36703779b7e323ca52277985582cf9c606870a11
junstyle <junstyle@git>
2024-04-11 13:00:56 +0800

    NamedArgumentExpression fix: https://github.com/junstyle/vscode-django-support/issues/21

--- src/print/NamedArgumentExpression.js
+++ src/print/NamedArgumentExpression.js
@@ -1,9 +1,9 @@
-import { concat } from './../util/prettier-doc-builders.js'
 import { STRING_NEEDS_QUOTES } from '../util'
+import { concat } from './../util/prettier-doc-builders.js'
 
 export const printNamedArgumentExpression = (node, path, print) => {
   node[STRING_NEEDS_QUOTES] = true
   const printedName = path.call(print, 'name')
   const printedValue = path.call(print, 'value')
-  return concat([printedName, ' = ', printedValue])
+  return concat([printedName, '=', printedValue])
 }
